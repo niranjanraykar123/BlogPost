a17c2d2f61600c96b19ed6d3084f9ab9da359d08
niranjan123 <raykarniranjan@gmail.com>
2024-05-26 14:42:54 +0530

    Deleting Reviews

--- controllers/review.js
+++ controllers/review.js
@@ -25,3 +25,16 @@ module.exports.createReview = async (req, res, next) => {
     next(err);
   }
 };
+
+//Deleting reviews
+module.exports.deleteReview = async (req, res) => {
+    let rev = await Review.findByIdAndDelete(req.params.reviewid);
+    console.log(rev);
+    let list = await Listing.findByIdAndUpdate(req.params.id, {
+      $pull: { reviews: req.params.reviewid },
+    });
+    req.flash("success", "Review Deleted");
+    console.log(list);
+    res.redirect(`/listings/${req.params.id}`);
+  };
+  
