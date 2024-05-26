a0db10ddceb10a5d7aecaa4884dcefd2d48010b6
niranjan123 <raykarniranjan@gmail.com>
2024-05-26 15:03:17 +0530

    Customized error class

---
+++ utils/Expresserror.js
@@ -0,0 +1,10 @@
+
+//Creating customized error class to handle errors
+class Expresserror extends Error {
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
+module.exports = Expresserror;
