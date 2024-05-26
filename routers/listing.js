dace418770870a376f8aaf3d4b8113a9dd85278e
niranjan123 <raykarniranjan@gmail.com>
2024-05-26 14:50:42 +0530

    Get and Post route for blogs

--- routers/listing.js
+++ routers/listing.js
@@ -1,12 +1,22 @@
-const express=require("express");
-const router=express.Router();
- const Listing=require("../models/listing.js")
+const express = require("express");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+let Expresserror = require("../utils/Expresserror.js");
+let { listingschema } = require("../schemajoi.js");
+let Review = require("../models/review.js");
+const { isLoggedIn, isOwner } = require("../middleware.js");
+const listingcontroller = require("../controllers/listing.js");
+const multer = require("multer");
 
-const listingcontroller=require("../controllers/listing.js");
-const multer  = require('multer')
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-
-router.route("/")
-.get(listingcontroller.index)
-
-module.exports=router;
+//Routes for getting all blogs and posting new blog 
+router
+  .route("/")
+  .get(listingcontroller.index)
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    listingcontroller.createListing
+  );
